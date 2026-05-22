3defb6ff2e3309a27cc7ba4607e1aeff710e77af
Midhun <midhunmohan107@gmail.com>
2026-05-22 12:53:05 +0530

    Write a program that takes a temperature in Fahrenheit as input and converts it to Celsius using the formula:
    celsius = (fahrenheit - 32) * 5/9
    
    Note : Comment the prompting messages
    
    like printf("enter the number ");

---
+++ temp convertion - f_c/temp convertion - f_c/Program.cs
@@ -0,0 +1,32 @@
+﻿/*using System.Xml.Linq;
+
+Write a program that takes a temperature in Fahrenheit as input and converts it to Celsius using the formula: 
+
+celsius = (fahrenheit - 32) * 5 / 9
+
+Note: Comment the prompting messages 
+
+like printf("enter the number ");*/
+
+
+using System;
+
+namespace temp
+{
+
+    class convert
+    {
+
+        static void Main(string[] arg)
+        {
+            float f,c;
+            Console.WriteLine("Enter he temp in Fahrentheit");
+
+            f = Convert.ToSingle(Console.ReadLine());
+
+            c = (f - 32) * (float)5 / 9;
+
+            Console.WriteLine($"Celsius {c}");
+        }
+    }
+}
