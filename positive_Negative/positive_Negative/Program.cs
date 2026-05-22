b4d48858c0ffbcaa5a3a711e5199a92da9710535
Midhun <midhunmohan107@gmail.com>
2026-05-22 15:24:01 +0530

    /*using System.Runtime.Intrinsics.X86;
    WAP to check whether a number is positive or negative
    
    Instructions:
    Use if else condition to check*/

---
+++ positive_Negative/positive_Negative/Program.cs
@@ -0,0 +1,41 @@
+﻿/*using System.Runtime.Intrinsics.X86;
+
+WAP to check whether a number is positive or negative
+
+Instructions:
+Use if else condition to check*/
+
+
+using System;
+
+namespace pos
+{
+
+    class neg
+    {
+        
+        static void Main(string[] arg)
+        {
+            int a;
+            while(true)
+            {
+                Console.WriteLine("Enter the number");
+                a = Convert.ToInt32(Console.ReadLine());
+                if (a == 0)
+                {
+                    Console.WriteLine("Number is Zero");
+                }
+                else if (a > 0)
+                {
+                    Console.WriteLine("Psivtie");
+                }
+                else
+                {
+                    Console.WriteLine("Negative");
+                }
+
+            }
+        }
+    }
+
+}
