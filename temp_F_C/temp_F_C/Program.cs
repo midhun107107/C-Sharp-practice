b0bc7e795650a0b38aa0d70742e0288adf33d9fa
Midhun <midhunmohan107@gmail.com>
2026-05-22 13:05:18 +0530

    T5 - Temperature Conversion - (int) Fahrenheit to Celcius (float)

---
+++ temp_F_C/temp_F_C/Program.cs
@@ -0,0 +1,29 @@
+﻿/*T5 - Temperature Conversion - (int)Fahrenheit to Celcius(float)*/
+
+
+using System;
+
+namespace temp
+{
+    class temp_conv
+    {
+
+        static void Main(string[] arg)
+        {
+            int Fahrenheit;
+            float Celcius;
+
+            Console.WriteLine("enter Fahrenheit");
+            
+            Fahrenheit = Convert.ToInt32(Console.ReadLine());
+
+            Celcius = (float)(Fahrenheit - 32) * (float)5 / 9;
+
+            Console.WriteLine($"Celcius { Celcius }" );
+
+
+
+        }
+    }
+
+}
