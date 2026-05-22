6b11a0e6bdf6bcdf5cbc69acf9024004fa42d3ba
Midhun <midhunmohan107@gmail.com>
2026-05-22 16:01:38 +0530

    /*using System.Runtime.Intrinsics.X86;
    WAP to check whether number is even or odd
    
    Instructions:
    Use if else condition*/

---
+++ odd_even/odd_even/Program.cs
@@ -0,0 +1,35 @@
+﻿/*using System.Runtime.Intrinsics.X86;
+
+WAP to check whether number is even or odd
+
+Instructions:
+Use if else condition*/
+
+using System;
+
+namespace odd
+{
+    class even
+    {
+
+        static void Main(String[] arg)
+        {
+            int a;
+
+            while (true)
+            {
+                Console.WriteLine("\nEnter a num");
+                a = Convert.ToInt32(Console.ReadLine());
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine("Even");
+                }
+                else
+                {
+                    Console.WriteLine("Odd");
+                }
+                
+            }
+        }
+    }
+}
